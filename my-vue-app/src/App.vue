da353517427e79d2520eacb6ff92be23988e42f5
Sofia <putlyuksophia@gmail.com>
2025-02-23 11:46:28 +0200

    props  training vue library

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,25 @@
+<script >
+import UserProfile from './components/UserProfile.vue';
+export default {
+  components:{
+   UserProfile
+  }
+}
+</script>
+
+<template>
+<div>
+<UserProfile 
+name="Настя" 
+:age=31
+imgAvatar="https://unba.org.ua/assets/uploads/publications/publikacii/2020-12-09-prosyanyuk_5fd0a7453c136.jpg"
+title="girl"
+city="Kyiv"
+:description="['Пише вірші','Читає','Малює']"
+/>
+
+ </div>
+</template>
+
+
+
---
+++ my-vue-app/src/components/UserProfile.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container-card">
+        <img :src="imgAvatar" :alt="title" class="image" />
+        <p>Ім'я: {{ name }}</p>
+        <p>Вік: {{ age }}</p>
+        <p>Місто : {{ city }}</p>
+        <ul class="container-list">
+            Хоббі:
+            <li v-for="(description, index) in description" :key="index" class="list">
+                {{ description }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        name: String,
+        age: Number,
+        imgAvatar: String,
+        title: String,
+        city: String,
+        description: Array
+    }
+}
+</script>
+<style>
+.container-card {
+    width: 300px;
+    height: 400px;
+    border-radius: 20px;
+    background-color: rgb(255, 207, 135);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+
+.image {
+    width: 200px;
+    margin-top: 30px;
+    border-radius: 20px;
+}
+
+.list {
+    list-style: none;
+    color:rgb(200, 86, 86)
+}
+.container-list {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap:10px;
+}
+</style>
